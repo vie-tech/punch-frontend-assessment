efac9c7bb9d51b1cdf0e613564470cd47025b294
Ativie Favour <ativiefavour03@gmail.com>
2024-06-11 02:40:03 +0100

    color-change

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -4,7 +4,7 @@ import jobCategories from "../config/job.config";
       <>
         <section className="grid grid-cols-1 justify-items-center my-10 bg-white">
           
-          <div className="w-3/5  font-semibold justify-items-center my-6 text-sm text-gray-600 bg-gray-200 rounded-lg">
+          <div className="w-3/5  font-semibold justify-items-center my-6 text-sm text-gray-600 bg-gray-100 rounded-lg">
            <div className="flex font-bold justify-center text-xs lg:text-sm p-3">
             <span className="bg-green-200 px-5 py-3 rounded-lg mr-[-10px] z-10">IT & Development</span>
             <span className="bg-white px-5 py-3 rounded-r-lg">Design and Creative</span>
