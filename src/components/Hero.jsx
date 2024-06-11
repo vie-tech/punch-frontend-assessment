21416f694fc431a1d25114dc26c1fac5859033f7
Ativie Favour <ativiefavour03@gmail.com>
2024-06-11 02:45:32 +0100

    image config

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,12 @@
 import searchButton from '../assets/images/Frame 626624.png'
-
+import gif from '../assets/images/gif.png'
 
 const Hero = () => {
   return (
     <>
       <section className="py-8 grid grid-cols-1 justify-items-center lg:mt-40"> {/* border here */}
        
-          <p className="text-5xl font-bold text-center my-8">Finding the right candidate <br /> has never been easier</p>
+          <p className="text-5xl font-bold text-center my-8 inline-block">Finding the right <span className='inline-block'><img src={gif} alt="" /></span>has <br /> never been easier</p>
           <p className="text-2xl text-gray-500 text-center">
             With our rigorous pre-vetting process, you'll never have to worry
             about <br /> finding the ideal candidate ever again.
